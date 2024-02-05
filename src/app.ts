189f3f8f2ca3085295f918aa10bda1f245051a28
Paul Sexton <github@psexton.net>
2024-02-05 13:25:52 -0600

    Add global boolean option with default

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import * as echo from "./commands/echo.ts"
 import * as add from "./commands/add.ts"
 
 await new Command()
+  .option("-s, --silent", "disable output.", { global: true, default: false })
   .command("echo", echo.command)
   .command("add", add.command)
   .parse(Deno.args);
