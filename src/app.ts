158c5e13f0b9b9f742a47966d3b121e02d4ab196
Paul Sexton <github@psexton.net>
2024-02-05 13:28:47 -0600

    Global option with global enum type

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
+import { Command, EnumType } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
+import { Animal } from "./types.ts"
 import * as echo from "./commands/echo.ts"
 import * as add from "./commands/add.ts"
 
 await new Command()
   .option("-s, --silent", "disable output.", { global: true, default: false })
+  .type("animal", new EnumType(Animal), { global: true })
+  .option(
+    "-a, --animal [name:animal]",
+    "Choose an animal.",
+    { global: true }
+  )
   .command("echo", echo.command)
   .command("add", add.command)
   .parse(Deno.args);
